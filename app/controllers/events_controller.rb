8beb7a1e43036b64195ddc46a94e6f447f955a35
chantellebecker <chantellewelp@gmail.com>
2017-04-23 13:29:01 -0400

    events controller formatting

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
 	before_action :set_event, only: [:show, :edit, :update, :destroy]
 	def new
-      	@event = Event.new
+    	@event = Event.new
     end
 
     def show
